46aab6000e34710fb8735b46195b8c2095f40ad1
Sam <sam-davidson1909@hotmail.co.uk>
2017-04-19 16:56:58 +0100

    Added my factors function file to git repository

---
+++ factors.py
@@ -0,0 +1,16 @@
+
+def factors(x):
+"""returns a list of factors of x"""
+    facs = {1,x}
+    t = x-1
+    r = 2
+    while t >= r:
+        if x % r == 0:
+            facs.add(r)
+            facs.add(int(x/r)) 
+            t = int(x/r)  
+        r += 1 
+    return facs    
+    
+    
+    

43fee3764946e4fd4bf8d9eb0ed15ceac20a8258
Sam <sam-davidson1909@hotmail.co.uk>
2017-04-19 17:34:28 +0100

    Removed two bugs, now no double factors are given

--- factors.py
+++ factors.py
@@ -1,17 +1,15 @@
-
 def factors(x):
-"""returns a list of factors of x"""
-    facs = {1,x}
+    if x == 1 : return [1]
+    facs = [1,x]
     t = x-1
     r = 2
-    while t >= r:
+    while t > r:
         if x % r == 0:
-            facs.add(r)
-            facs.add(int(x/r)) 
+            facs.append(r)
+            if r != int(x/r) : facs.append(int(x/r)) 
             t = int(x/r)  
         r += 1 
     facs.sort()
-    return facs    
-    
+    return facs   
     
     
